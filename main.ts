1244361a904ba5bad0a6f7069116647139cf3998
Tsukitai <103410207+Tsukitai@users.noreply.github.com>
2022-05-09 09:26:13 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (led.brightness()))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
